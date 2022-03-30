7befc8fa1b22a344fce0136bc07ad763213e9fb0
enderghast13 <29264120+enderghast13@users.noreply.github.com>
2022-03-30 16:18:41 -0700

    nxdt_host.py: update license

--- host/nxdt_host.py
+++ host/nxdt_host.py
@@ -7,17 +7,18 @@
  *
  * This file is part of nxdumptool (https://github.com/DarkMatterCore/nxdumptool).
  *
- * nxdumptool is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
+ * nxdumptool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * nxdumptool is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * nxdumptool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 # This script depends on PyUSB and tqdm.
