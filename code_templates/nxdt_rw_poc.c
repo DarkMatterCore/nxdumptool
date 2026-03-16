0a4c80bd0e2299e8b01dcfe9da9b96f1d9ddebb3
Andrew Vineyard <dimensional42@gmail.com>
2026-03-16 11:55:37 -0500

    Enhance NSP queue menu interaction and button handling

--- code_templates/nxdt_rw_poc.c
+++ code_templates/nxdt_rw_poc.c
@@ -1432,6 +1432,8 @@ int main(int argc, char *argv[])
                 consolePrint("publisher: %s\n", app_metadata->publisher);
                 if (cur_menu->id == MenuId_UserTitlesSubMenu || cur_menu->id == MenuId_NspTitleTypes || cur_menu->id == MenuId_TicketTitleTypes || \
                     cur_menu->id == MenuId_NcaTitleTypes) consolePrint("title id: %016lX\n", app_metadata->title_id);
+                if (cur_menu->id == MenuId_NspTitleTypes || cur_menu->id == MenuId_Nsp)
+                    consolePrint("press zr to enter the nsp queue menu\n");
                 consolePrint("______________________________\n\n");
             }
 
@@ -1574,7 +1576,7 @@ int main(int argc, char *argv[])
 
         if (data_update) continue;
 
-        bool is_nsp_queue_menu_btn_down = ((btn_down & HidNpadButton_ZR) && cur_menu->id == MenuId_UserTitles && element_count);
+        bool is_nsp_queue_menu_btn_down = ((btn_down & HidNpadButton_ZR) && ((cur_menu->id == MenuId_UserTitles) || (cur_menu->id >= MenuId_NspTitleTypes)) && element_count);
 
         if (((btn_down & HidNpadButton_A) && selected_element) || is_nsp_queue_menu_btn_down)
         {
@@ -1677,7 +1679,7 @@ int main(int argc, char *argv[])
 
                 if (!error)
                 {
-                    child_menu->parent = (is_nsp_queue_menu_btn_down ? &g_userTitlesMenu : cur_menu);
+                    child_menu->parent = cur_menu;
                     cur_menu = child_menu;
                     element_count = menuGetElementCount(cur_menu);
                 } else {
